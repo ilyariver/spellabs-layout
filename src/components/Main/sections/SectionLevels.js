e171bedee02544ddb69210980ca8c3b8e788ef56
bucks20007 <bucks20007@gmail.com>
2021-11-13 01:11:58 +0300

    Finished level component

--- src/components/Main/sections/SectionLevels.js
+++ src/components/Main/sections/SectionLevels.js
@@ -5,7 +5,7 @@ import {mediaQueries} from '../../../globalContainer';
 import gridBreakpoints from '../../../cssVar';
 
 // Размеры устройств
-const {md, lg, xl} = gridBreakpoints;
+const {sm, md, lg, xl} = gridBreakpoints;
 
 // Обертка секции
 const SectionLevelsWrap = styled.section`
@@ -17,23 +17,22 @@ const SectionLevelsWrap = styled.section`
 
   .section-container {
     position: relative;
-
   }
 
   ${mediaQueries(md, `
   	padding-top: 3rem;
-  padding-bottom: 7rem;
-
-  .section-container {
-    &:after {
-      bottom: -61rem;
-      left: -52rem;
-    }
-  }	
-  	`)}
+		padding-bottom: 7rem;
+	
+		.section-container {
+			&:after {
+				bottom: -61rem;
+				left: -52rem;
+			}
+		}	
+	`)}
 
   ${mediaQueries(lg, `
-  padding-top: 100px;
+  	padding-top: 100px;
     padding-bottom: 200px;
 
     .section-container {
@@ -45,26 +44,24 @@ const SectionLevelsWrap = styled.section`
         transform: rotate(32deg);
       }
     }
-  	`)}
+	`)}
 
   ${mediaQueries(xl, `
-  padding-top: 140px;
-    padding-bottom: 215px;
+  	padding-top: 76px;
+    padding-bottom: 80px;
 
     .section-container {
       &:after {
         left: -1320px;
       }
     }
-  	`)}
-
+	`)}
 `;
 
 // Заголовок секции
-const SectionTitle = styled.h1`
+const SectionTitle = styled.h2`
   position: relative;
   margin: 0 0 3rem;
-  width: 27rem;
   font-size: 3rem;
   line-height: 3.8rem;
   color: var(--color-white);
@@ -75,32 +72,30 @@ const SectionTitle = styled.h1`
     font-weight: 600;
   }
 
-  @media (min-width: ${gridBreakpoints.sm}) {
-    width: 21rem;
-    font-size: 2rem;
-    line-height: 3rem;
-  }
+  ${mediaQueries(sm, `
+		font-size: 2.5rem;
+		line-height: 3rem;
+	`)}
 
-  @media (min-width: ${gridBreakpoints.md}) {
-    width: 16rem;
-    margin: 0 0 1rem;
-    font-size: 1.5rem;
-    line-height: 2rem;
-  }
+  ${mediaQueries(md, `
+  	width: 20rem;
+		margin: 0 0 1.5rem 0;
+		font-size: 1.5rem;
+		line-height: 2rem;
+ `)}
 
-  @media (min-width: ${gridBreakpoints.lg}) {
-    width: 580px;
-    margin: 0 0 30px;
-    font-size: 55px;
-    line-height: 70px;
-  }
-
-  @media (min-width: ${gridBreakpoints.lg}) {
-    width: 510px;
-    margin: 0 0 30px;
+  ${mediaQueries(lg, `
+  	width: 620px;
+		margin: 0 0 50px;
     font-size: 48px;
     line-height: 59px;
-  }
+	 `)}
+
+  ${mediaQueries(xl, `
+		margin: 0 0 55px;
+		font-size: 48px;
+		line-height: 59px;
+	`)}
 `;
 
 // Список уровней
@@ -109,6 +104,13 @@ const LevelList = styled.ul`
   flex-direction: column;
   align-items: center;
   color: var(--color-white);
+
+  ${mediaQueries(md, `
+		flex-direction: row;
+  	align-items: start;
+  	justify-content: space-between;
+  	flex-wrap: wrap;
+	`)}	
 `;
 
 // Пункты списка
@@ -116,37 +118,205 @@ const LevelItem = styled.li`
   background-color: var(--color-blue-light);
   width: 26rem;
   height: 30rem;
-  padding: 3.4rem;
-  border-radius: 5px;
+  padding: 2rem;
+  border-radius: 0.7rem;
 
   &:not(:last-child) {
     margin-bottom: 2rem;
   }
-  
-  &. .triangle {
+
+  .figure {
+    margin-bottom: 1.6rem;
+  }
+
+  .triangle {
+    position: relative;
     width: 0;
     height: 0;
     border-style: solid;
-    border-width: 0 0 30px 30px;
-    border-color: transparent transparent #007bff transparent;
+    border-width: 0 0 39px 39px;
+    border-color: transparent transparent var(--color-white) transparent;
+		transition: border 0.1s ease-in-out;
+
+    &:after {
+      content: "";
+      position: absolute;
+      top: 11px;
+      left: -28px;
+      width: 0;
+      height: 0;
+      border-style: solid;
+      border-width: 0 0 23px 23px;
+      border-color: transparent transparent var(--color-blue-light) transparent;
+      z-index: 2;
+    }
   }
+
+  .circle {
+    width: 35px;
+    height: 35px;
+    background-color: var(--color-blue-light);
+    border-radius: 50%;
+    border: 5px solid var(--color-white);
+		transition: border 0.1s ease-in-out;
+  }
+
+  .square {
+    width: 35px;
+    height: 35px;
+    background-color: var(--color-blue-light);
+    border: 5px solid var(--color-white);
+		transition: border 0.1s ease-in-out;
+  }
+
+  ${mediaQueries(sm, `
+		width: 19rem;
+		height: 22rem;
+		padding: 1.8rem;
+		border-radius: 0.5rem;	
+		
+		.figure {
+			margin-bottom: 1.2rem;
+		}
+	`)}  
+
+  ${mediaQueries(md, `
+		width: calc(100% / 2 - 0.4rem);
+    height: 15rem;
+    padding: 1.2rem;
+    border-radius: 0.3rem;
+    border: 5px solid var(--color-blue-light);
+    transition: border 0.1s ease-in-out;
+		
+    &:not(:last-child) {
+			margin-bottom: 0.8rem;
+		}
+		
+		&:last-child {
+			margin: 0 auto;
+			margin-bottom: 0.8rem;
+		}
+		    
+    .figure {
+			margin-bottom: 0.9rem;
+		}
+		
+		&:hover {
+			border: 5px solid var(--color-yellow-font);
+			.circle, .square {
+				border: 5px solid var(--color-yellow-font);
+			}
+			.triangle {
+				border-color: transparent transparent var(--color-yellow-font) transparent;
+			}
+		}
+	`)}
+
+  ${mediaQueries(lg, `
+		width: calc(100% / 3 - 10px);
+		height: 350px;
+    border-radius: 8px;
+		padding: 30px;		
+		
+		&:not(:last-child) {
+			margin-bottom: 15px;
+		}
+		
+		&:last-child {
+			margin: 0;
+			margin-bottom: 15px;
+		}
+		
+		.figure {
+			margin-bottom: 20px;
+		}
+	`)}
+
+  ${mediaQueries(xl, `
+		width: calc(100% / 3 - 10px);
+		height: 465px;
+    border-radius: 10px;
+		padding: 35px 40px;
+    
+    .figure {
+			margin-bottom: 25px;
+		}	
+		
+		.triangle {
+			border-width: 0 0 45px 45px;
+
+			&:after {
+				top: 10px;
+				left: -35px;
+				border-width: 0 0 30px 30px;
+			}
+		}
+				
+		.square {
+			width: 46px;
+			height: 46px;
+		}
+	
+		.circle {
+			width: 46px;
+			height: 46px;
+		}	
+	`)}
 `;
 
-// Фигура в пункте
-// const Figure = styled.div`
-// `;
-
 // Заголовок
-const Title = styled.h5`
-  margin: 0 0 15px 0;
-  font-size: 14px;
+const Title = styled.h3`
+  margin: 0 0 1.5rem 0;
+  font-size: 1.4rem;
+
+  ${mediaQueries(sm, `
+		margin: 0 0 0.5rem 0;
+    font-size: 0.9rem;
+	`)}
+
+  ${mediaQueries(md, `
+		margin: 0 0 0.5rem 0;
+    font-size: 0.6rem;
+	`)}
+
+  ${mediaQueries(lg, `
+		margin: 0 0 10px 0;
+    font-size: 12px;
+	`)}
+
+  ${mediaQueries(xl, `
+		margin: 0 0 12px 0;
+    font-size: 20px;
+	`)}
 `;
 
 // Текст
 const Text = styled.p`
   font-weight: 400;
-  font-size: 12px;
-  line-height: 20px;
+  font-size: 1.2rem;
+  line-height: 2rem;
+
+  ${mediaQueries(sm, `
+  	margin-top: 0;
+		font-size: 0.7rem;
+  	line-height: 1.2rem;
+	`)}
+
+  ${mediaQueries(md, `
+		font-size: 0.5rem;
+  	line-height: 0.8rem;
+	`)}
+
+  ${mediaQueries(lg, `
+		margin: 0;
+    font-size: 11px;
+  	line-height: 20px;
+	`)}
+
+  ${mediaQueries(xl, `
+		line-height: 25px;
+    font-size: 16px;
+	`)}
 `;
 
 // Компонент секции "Уровни"
@@ -190,7 +360,7 @@ class SectionLevels extends React.Component {
 							this.state.levels.map(item => {
 								return (
 									<LevelItem key={item.title}>
-										{/*<div className={item.figure}></div>*/}
+										<div className={'figure ' + item.figure}></div>
 										<Title>{item.title}</Title>
 										<Text>{item.text}</Text>
 									</LevelItem>
